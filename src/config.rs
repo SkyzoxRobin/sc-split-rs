1804673dd1e49948da3abbcd229fb2ecba487be7
Skyzox <robinskyzox@gmail.com>
2021-10-20 17:41:21 +0200

    upgrade to 0.20

--- src/config.rs
+++ src/config.rs
@@ -3,13 +3,4 @@ elrond_wasm::imports!();
 
 #[elrond_wasm_derive::module]
 pub trait ConfigModule: {
-    
-    #[view(getDurationInBlocks)]
-    #[storage_mapper("duration_in_blocks")]
-    fn duration_in_blocks(&self) -> SingleValueMapper<Self::Storage, Self::BigUint>;
-    
-    #[view(getDurationInSeconds)]
-    #[storage_mapper("duration_in_seconds")]
-    fn duration_in_seconds(&self) -> SingleValueMapper<Self::Storage, Self::BigUint>;
-
 }
